2f0933fd0f78003d33eebbbd52919b09381444a2
Kris Leech <kris.leech@interkonect.com>
2011-06-15 09:49:22 +0100

    Upgrade Rails version and lock gems to Rails 2 compatible versions

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -18,18 +18,16 @@ Rails::Initializer.run do |config|
   config.gem "qcore", :version => '~>1.0'
   config.gem "qcms",  :version => '~>1.0'
   config.gem "path_finder",  :version => '~>1.0'
-  config.gem "backup_database",  :version => '~>1.0'
 
   # Dependancies
-  config.gem 'slim_scrooge'
   config.gem "acts_as_list"
   config.gem "acts_as_tree"
-  config.gem 'authlogic'
-  config.gem 'will_paginate'
+  config.gem 'authlogic', :version => '2.1.6'
+  config.gem 'will_paginate', :version => '~> 2.3.11'
   config.gem 'RedCloth'
   config.gem "state_machine", :version => '0.8.0'
-  config.gem "settingslogic"
-  config.gem "searchlogic", :version => "2.0.0"
+  config.gem "settingslogic", :version => '~>2.0'
+  config.gem "searchlogic", :version => "~>2.0"
   config.gem "sanitize"
   config.gem "paperclip"
 
