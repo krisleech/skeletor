41cbb4c5cdcac3cc29fc858473ca49bd8611180b
Quy Doan <quydoantran@gmail.com>
2011-08-30 21:14:27 +0800

    use bundler to manage gems

---
+++ Gemfile
@@ -0,0 +1,30 @@
+#http://yehudakatz.com/2011/05/30/gem-versioning-and-bundler-doing-it-right/
+source :gemcutter
+
+gem 'rails', '2.3.11', :require => nil
+
+gem 'mysql'
+
+gem "qcore", '~>1.0'
+gem "qcms", '~>1.0'
+gem "path_finder", '~>1.0'
+
+# Dependancies
+gem "acts_as_list"
+gem "acts_as_tree"
+gem 'formtastic', '0.9.7'
+gem 'authlogic', '2.1.6'
+gem 'will_paginate', '~> 2.3.11'
+gem 'RedCloth'
+gem "state_machine", '0.8.0'
+gem "settingslogic", '~>2.0'
+gem "searchlogic", "~>2.0"
+gem "sanitize"
+gem "paperclip", "2.3.11"
+gem 'inherited_resources', '1.0.0'
+gem 'responders', "0.4.0"
+
+group :development do
+ gem "rspec", '1.3.2', :require => 'spec'
+ gem "rspec-rails", '1.3.2', :require => false
+end
--- config/environment.rb
+++ config/environment.rb
@@ -6,43 +6,19 @@ RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.3.7')
+  module Rails
+    class GemDependency
+      def requirement
+        r = super
+        (r == Gem::Requirement.default) ? nil : r
+      end
+    end
+  end
+end
+
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Qwerty
-  config.gem "qcore", :version => '~>1.0'
-  config.gem "qcms",  :version => '~>1.0'
-  config.gem "path_finder",  :version => '~>1.0'
-
-  # Dependancies
-  config.gem "acts_as_list"
-  config.gem "acts_as_tree"
-  config.gem 'authlogic', :version => '2.1.6'
-  config.gem 'will_paginate', :version => '~> 2.3.11'
-  config.gem 'RedCloth'
-  config.gem "state_machine", :version => '0.8.0'
-  config.gem "settingslogic", :version => '~>2.0'
-  config.gem "searchlogic", :version => "~>2.0"
-  config.gem "sanitize"
-  config.gem "paperclip"
-  config.gem 'inherited_resources', :version => '~>1.0'
-
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
+ 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
